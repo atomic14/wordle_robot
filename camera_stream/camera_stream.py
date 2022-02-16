5cdb553e8ce6ed254b9b9b2c6cafdd21fc2f021b
Chris Greening <chris@cmgresearch.com>
2022-02-16 09:54:39 +0000

    iAdds camera streaming module

---
+++ camera_stream/camera_stream.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from flask import Flask,send_file
+
+app = Flask(__name__)
+
+camera = None
+
+@app.route('/grab_frame',methods = ['GET'])
+def grab_frame():
+    camera.capture('test.jpg')
+    return send_file('test.jpg')
+
+if __name__ == '__main__':
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    app.run(host='0.0.0.0', port = 8000, threaded = True, debug = True)
+
