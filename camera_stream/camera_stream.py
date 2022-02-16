29de701dbe83f33fe8bcfa21ba4290b52eab5fbb
Chris Greening <chris@cmgresearch.com>
2022-02-16 14:06:16 +0000

    Pre-commit hook run

--- camera_stream/camera_stream.py
+++ camera_stream/camera_stream.py
@@ -1,17 +1,18 @@
 from picamera import PiCamera
-from flask import Flask,send_file
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 camera = None
 
-@app.route('/grab_frame',methods = ['GET'])
-def grab_frame():
-    camera.capture('test.jpg')
-    return send_file('test.jpg')
 
-if __name__ == '__main__':
+@app.route("/grab_frame", methods=["GET"])
+def grab_frame():
+    camera.capture("test.jpg")
+    return send_file("test.jpg")
+
+
+if __name__ == "__main__":
     camera = PiCamera()
     camera.resolution = (1024, 768)
-    app.run(host='0.0.0.0', port = 8000, threaded = True, debug = True)
-
+    app.run(host="0.0.0.0", port=8000, threaded=True, debug=True)
